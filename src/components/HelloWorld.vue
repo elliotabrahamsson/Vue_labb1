a66a76528d39b029bc68c0cfd8b8d393e971ea87
elliotabrahamsson <elliotsson@outlook.com>
2025-01-29 15:01:30 +0100

    started oroject. Implemented tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 defineProps({
   msg: String,
-})
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -19,7 +19,7 @@ const count = ref(0)
     </p>
   </div>
 
-  <p>
+  <p class="bg-amber-100">
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
