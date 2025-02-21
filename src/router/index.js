8e84766471d3d4d29375479737ab49232fce30a0
elliotabrahamsson <elliotsson@outlook.com>
2025-02-21 11:20:12 +0100

    Cleaned up the project a bit. Changed name to PreviewView and deleted unnecessities

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "../views/HomeView.vue";
 import ErrorPage from "../components/ErrorPage.vue";
-import AddOlympiaView from "../views/AddOlympiaView.vue";
+import PreviewView from "../views/PreviewView.vue";
 
 const routes = [
   { path: "/", name: "Home", component: HomeView },
   { path: "/:pathmatch(.*)", name: "Error", component: ErrorPage },
-  { path: "/Add_Olympia", name: "Add Olympia", component: AddOlympiaView },
+  { path: "/Add_Olympia", name: "Add Olympia", component: PreviewView },
 ];
 
 const router = createRouter({ history: createWebHistory(), routes });
